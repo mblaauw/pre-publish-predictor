717d18721ec6b994d23ca238ae094a421089c38d
mblaauw <michel.blaauw@gmail.com>
2014-01-29 14:41:37 +0100

    under construction

--- web/ppp/admin.py
+++ web/ppp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from ppp.models import test
+
+
+admin.site.register(test)
+
--- web/ppp/models.py
+++ web/ppp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class test(models.Model):
+    print('These are your classes as you start building real apps')
